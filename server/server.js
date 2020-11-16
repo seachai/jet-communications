fa7dfc9e0b5944f40c6847f0460246a1e86354c7
James Scaggs <james.scaggs@gmail.com>
2020-11-16 15:27:12 -0600

    init commit

---
+++ server/server.js
@@ -0,0 +1,75 @@
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+// SET UP ENV VARIABLES
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+// server
+const server = express();
+const PORT = process.env.PORT || 3000;
+
+// DATABASE
+const MONGO_URI = process.env.MONGO_URI;
+// mongoose
+//   .connect(MONGO_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     // sets the name of the DB that our collections are part of
+//     dbName: "tasselled-wobegong",
+//   })
+//   .then(() => console.log("Connected to Mongo DB."))
+//   .catch((err) => console.log(err));
+
+// SET UP
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
+
+// // API ROUTER
+// const apiRouter = require("./routes/api");
+
+// // SEND API CALLS TO API ROUTER
+// server.use("/api", apiRouter);
+
+// REGULAR ROUTES
+server.get("/", (req, res) => {
+  res.status(200).json({ message: "hello" });
+});
+
+server.get("/welcome", (req, res) => {
+  res.status(200).json({ message: "Successfully authenticated" });
+});
+
+// ERROR HANDLER
+server.use((err, req, res, next) => {
+  const defaultErr = {
+    log: "Express error: Unknown middleware",
+    status: 500,
+    message: { err: "An error occurred" },
+  };
+  const errorObj = { ...defaultErr, err };
+
+  console.log("ERROR LOG => ", errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+// statically serve everything in the build folder on the route '/build'
+if (process.env.NODE_ENV === "production") {
+  server.use("/build", express.static(path.join(__dirname, "../build")));
+  // serve index.html on the route '/'
+  server.use("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./index.html"));
+  });
+}
+
+server.listen(PORT);
+
+console.log("NODE_ENV mode is", process.env.NODE_ENV);
+
+console.log("Remember to check your .env file if you cannot connect to the database");
+
+console.log(`Server is listening at http://localhost:${PORT}`);
+console.log(`Client is live at http://localhost:8080`);
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const webpack = require("webpack");
+const Dotenv = require("dotenv-webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: process.env.NODE_ENV || "production",
+  entry: "./src/index.tsx",
+  devtool: "inline-source-map",
+  devServer: {
+    hot: true,
+    compress: true,
+    port: 8080,
+    proxy: {
+      "/api": "http://localhost:3000",
+    },
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".tsx", ".js", ".jsx"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "index.html",
+      inject: "body",
+    }),
+    new Dotenv({
+      safe: true, // load '.env.example' to verify the '.env' variables are all set. Can also be a string to a different file.
+      allowEmptyValues: true, // allow empty variables (e.g. `FOO=`) (treat it as empty string, rather than missing)
+      systemvars: true, // load all the predefined 'process.env' variables which will trump anything local per dotenv specs.
+      silent: true, // hide any errors
+      defaults: false, // load '.env.defaults' as the default values if empty.
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
