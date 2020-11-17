4760f6a9daf431a14496839cb6f5835a2544cdec
James Scaggs <james.scaggs@gmail.com>
2020-11-17 10:05:57 -0600

    scaffold out conversations and video chat endopints

--- server/routes/api.js
+++ server/routes/api.js
@@ -6,4 +6,49 @@ router.get("/", (req, res) => {
   res.status(200).json({ message: "/api route ping" });
 });
 
+// USERS
+// create user
+router.post("/users", async (req, res, next) => {
+  try {
+    // TODO: Create a user in the database
+    res.status(200);
+  } catch (error) {
+    console.log({ error });
+    return next({
+      log: `Error caught in POST users. \n Error Message: ${e.errmsg || e}`,
+      message: { err: e.errmsg || e },
+    });
+  }
+});
+
+// CONVERSATIONS
+// create conversation
+router.post("/conversations", async (req, res, next) => {
+  try {
+    // TODO: Create a conversation in the database
+    res.status(200);
+  } catch (error) {
+    console.log({ error });
+    return next({
+      log: `Error caught in POST conversations. \n Error Message: ${e.errmsg || e}`,
+      message: { err: e.errmsg || e },
+    });
+  }
+});
+
+// VIDEO
+router.post("/video-chat", async (req, res, next) => {
+  try {
+    // TODO: Insantiate a video chat on Twilio
+  } catch (error) {
+    console.log({ error });
+    return next({
+      log: `Error caught in POST video-chat. \n Error Message: ${e.errmsg || e}`,
+      message: { err: e.errmsg || e },
+    });
+  }
+});
+
+// WHATSAPP
+
 module.exports = router;
