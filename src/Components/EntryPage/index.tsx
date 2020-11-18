fc4f424a18ad5962339f5bbc32a34d803d4f9f4d
seachai <imchai@gmail.com>
2020-11-17 20:16:21 -0500

    Added style changes

--- src/Components/EntryPage/index.tsx
+++ src/Components/EntryPage/index.tsx
@@ -6,12 +6,8 @@ import UserForms from "./UserForms";
 
 const EntryPage = () => {
   const { isLoggedIn } = useContext(AuthContext);
-  // Render Register if the user doesn't have the auth cookie
-
-  // Else redirect the user directly to the App
   return (
     <div className='EntryPage'>
-      <h1>EntryPage</h1>
       {isLoggedIn ? <Redirect to='/chatroom' /> : <UserForms />}
     </div>
   );
