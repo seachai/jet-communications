9009511281012912da11777aef2a3a463d019af6
James Scaggs <james.scaggs@gmail.com>
2020-11-17 19:00:56 -0600

    token request finished

--- src/Components/VideoChat/index.tsx
+++ src/Components/VideoChat/index.tsx
@@ -5,14 +5,15 @@ import Lobby from "./Lobby";
 import axios from "axios";
 
 export default function VideoChat() {
-  const auth = useContext(AuthContext);
+  const { auth } = useContext(AuthContext);
   const [token, setToken] = useState(null);
 
   useEffect(() => {
     async function getToken() {
       const endpoint = `${process.env.REACT_APP_API_URL}/video/token`;
       const { data } = await axios.post(endpoint, { identity: auth.name, room: Date.now() });
-      setToken(data.token);
+
+      setToken(data);
     }
 
     getToken();
